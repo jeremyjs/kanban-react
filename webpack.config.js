4cb752f5113cec1486d612c6c4808da2bc692b7b
Jeremy Meyer <jjman505@gmail.com>
2015-12-12 17:42:37 +0900

    updated webpack config to be modular based on env

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,52 @@
 
-var path = require('path')
-  , webpack = require('webpack')
-  , HtmlWebpackPlugin = require('html-webpack-plugin')
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var merge = require('webpack-merge');
 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
-  app:   path.resolve(__dirname, 'app')
-, build: path.resolve(__dirname, 'build')
+  app:   path.resolve(__dirname, 'app'),
+  build: path.resolve(__dirname, 'build'),
 };
 
-var HtmlKanbanPlugin = new HtmlWebpackPlugin({ title: 'Kanban' })
-  , HMR_Plugin = new webpack.HotModuleReplacementPlugin()
+var HtmlKanbanPlugin = new HtmlWebpackPlugin({ title: 'Kanban' });
+var HMR_Plugin = new webpack.HotModuleReplacementPlugin();
 
-module.exports = {
+var common = {
   entry: PATHS.app,
-  output: {
-    path: PATHS.build
-  , filename: 'bundle.js'
-  }
-, module: {
+  // output: {
+  //   path: PATHS.build,
+  //   filename: 'bundle.js',
+  // },
+  module: {
     loaders: [
       {
-        test: /\.css$/
-      , loaders: ['style', 'css']
-      , include: PATHS.app
-      }
-    ]
-  }
-, devServer: {
-    historyApiFallback: true
-  , hot: true
-  , inline: true
-  , progress: true
-  , stats: 'errors-only'
-  , host: process.env.HOST
-  , port: process.env.PORT
-  }
-, plugins: [
-    HtmlKanbanPlugin
-  , HMR_Plugin
-  ]
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app,
+      },
+    ],
+  },
+  plugins: [
+    HtmlKanbanPlugin,
+  ],
 };
+
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      stats: 'errors-only',
+      host: process.env.HOST,
+      port: process.env.PORT,
+    },
+    plugins: [
+      HMR_Plugin,
+    ],
+  });
+}
