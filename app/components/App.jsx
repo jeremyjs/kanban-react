935b0f45f65fdf62166f0f84465d4fa29a010170
Jeremy Meyer <jjman505@gmail.com>
2015-12-18 17:21:34 -0500

    assign tasks using state

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -4,11 +4,21 @@ import TaskList from './TaskList.jsx';
 import { tasks } from '../fixtures.js';
 
 export default class App extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      tasks: tasks,
+    };
+  };
+
   render () {
+    const tasks = this.state.tasks;
+
     return (
       <div className="app">
         <TaskList tasks={tasks}/>
       </div>
     );
-  }
+  };
 };
