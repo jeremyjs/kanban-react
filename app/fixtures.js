24021945809c1c82f1f88fb3136a540bf3785e43
Jeremy Meyer <jjman505@gmail.com>
2015-12-18 16:41:26 -0500

    updated task schema

--- app/fixtures.js
+++ app/fixtures.js
@@ -2,16 +2,16 @@
 module.exports = {
   tasks: [
     {
-      id: '4a068c42-75b2-4ae2-bd0d-284b4abbb8f0',
-      task: 'Learn Webpack',
+      _id: '4a068c42-75b2-4ae2-bd0d-284b4abbb8f0',
+      title: 'Learn Webpack',
     },
     {
-      id: '4e81fc6e-bfb6-419b-93e5-0242fb6f3f6a',
-      task: 'Learn React',
+      _id: '4e81fc6e-bfb6-419b-93e5-0242fb6f3f6a',
+      title: 'Learn React',
     },
     {
-      id: '11bbffc8-5891-4b45-b9ea-5c99aadf870f',
-      task: 'Do laundry',
+      _id: '11bbffc8-5891-4b45-b9ea-5c99aadf870f',
+      title: 'Do laundry',
     },
   ],
 };
