eb237ba1adbcf040597252bd64e250b1d3aa1353
Jeremy Meyer <jjman505@gmail.com>
2015-12-21 07:16:35 -0500

    added Task class for tasks

--- app/fixtures.js
+++ app/fixtures.js
@@ -1,19 +1,18 @@
 
 var uuid = require('node-uuid');
 
+class Task {
+  constructor (title) {
+    this._id   = uuid();
+    this.title = title;
+  }
+}
+
 module.exports = {
+  Task: Task,
   tasks: [
-    {
-      _id: uuid(),
-      title: 'Learn Webpack',
-    },
-    {
-      _id: uuid(),
-      title: 'Learn React',
-    },
-    {
-      _id: uuid(),
-      title: 'Get shwifty',
-    },
+    new Task('Learn Webpack'),
+    new Task('Learn React'),
+    new Task('Get shwifty'),
   ],
 };
