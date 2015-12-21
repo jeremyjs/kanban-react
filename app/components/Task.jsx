048f2791b21496308757520657ad9c5da76a30b5
Jeremy Meyer <jjman505@gmail.com>
2015-12-21 07:17:23 -0500

    enable editing of tasks

--- app/components/Task.jsx
+++ app/components/Task.jsx
@@ -2,7 +2,35 @@
 import React from 'react';
 
 export default class Task extends React.Component {
-  render () {
-    return <div className="task">{this.props.title}</div>;
+  constructor (props) {
+    super(props);
+    this.state = { isEditing: false };
+  };
+
+  render = () => {
+    return this.state.isEditing ? this.renderEdit() : this.renderTask();
+  };
+
+  renderEdit = () => {
+    return <input type="text"
+                  autoFocus={true}
+                  defaultValue={this.props.title}
+                  onBlur={this.saveEdits}
+                  onKeyPress={this.checkEnter} />;
+  };
+
+  renderTask = () => {
+    return <div className="task" onClick={this.edit}>{this.props.title}</div>;
+  };
+
+  edit = () => this.setState({ isEditing: true });
+
+  checkEnter = (e) => {
+    if(e.key === 'Enter') this.saveEdits(e);
+  };
+
+  saveEdits = (e) => {
+    // this.props.onSaveEdits(e.target.value);
+    this.setState({ isEditing: false });
   };
 }
